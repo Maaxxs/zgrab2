e5fbe5d5ff4d69779ede1ed8c4a1739128cac100
Max Kunzelmann <maxdev@posteo.de>
2024-10-08 18:55:20 +0200

    fix: add missing import

--- scanner.go
+++ scanner.go
@@ -3,7 +3,7 @@ package zgrab2
 import (
 	"context"
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus" // original import was just "log"
 	"time"
 )
 
@@ -33,7 +33,7 @@ func RegisterScan(name string, s Scanner) {
 	}
 	defaultDialerGroupToScanners[name] = dialerGroup
 	scanners[name] = &s
-	log.Infof("Registered scanner: %s ", name)
+	log.Debugf("Registered scanner: %s ", name)
 }
 
 // PrintScanners prints all registered scanners
