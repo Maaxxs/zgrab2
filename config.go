a716962acb2e0a3cc9ee040412bdae7d80cadef0
nlohmill <nils.lohmiller@hs-esslingen.de>
2025-06-23 13:36:13 +0200

    add config

--- config.go
+++ config.go
@@ -24,27 +24,39 @@ const (
 // Config is the high level framework options that will be parsed
 // from the command line
 type Config struct {
-	LocalAddrString    string          `long:"source-ip" default:"" description:"Set as local source IP."`
-	OutputFileName     string          `short:"o" long:"output-file" default:"-" description:"Output filename, use - for stdout"`
-	InputFileName      string          `short:"f" long:"input-file" default:"-" description:"Input filename, use - for stdin"`
-	MetaFileName       string          `short:"m" long:"metadata-file" default:"-" description:"Metadata filename, use - for stderr"`
-	LogFileName        string          `short:"l" long:"log-file" default:"-" description:"Log filename, use - for stderr"`
-	Senders            int             `short:"s" long:"senders" default:"1000" description:"Number of send goroutines to use"`
-	Debug              bool            `long:"debug" description:"Include debug fields in the output."`
-	Flush              bool            `long:"flush" description:"Flush after each line of output."`
-	GOMAXPROCS         int             `long:"gomaxprocs" default:"0" description:"Set GOMAXPROCS"`
-	ConnectionsPerHost int             `long:"connections-per-host" default:"1" description:"Number of times to connect to each host (results in more output)"`
-	ReadLimitPerHost   int             `long:"read-limit-per-host" default:"96" description:"Maximum total kilobytes to read for a single host (default 96kb)"`
-	Prometheus         string          `long:"prometheus" description:"Address to use for Prometheus server (e.g. localhost:8080). If empty, Prometheus is disabled."`
-	CustomDNS          string          `long:"dns" description:"Address of a custom DNS server for lookups. Default port is 53."`
-	Multiple           MultipleCommand `command:"multiple" description:"Multiple module actions"`
-	inputFile          *os.File
-	outputFile         *os.File
-	metaFile           *os.File
-	logFile            *os.File
-	inputTargets       InputTargetsFunc
-	outputResults      OutputResultsFunc
-	localAddr          *net.TCPAddr
+	LocalAddrString       string          `long:"source-ip" default:"" description:"Set as local source IP."`
+	OutputFileName        string          `short:"o" long:"output-file" default:"-" description:"Output filename, use - for stdout"`
+	InputFileName         string          `short:"f" long:"input-file" default:"-" description:"Input filename, use - for stdin"`
+	MetaFileName          string          `short:"m" long:"metadata-file" default:"-" description:"Metadata filename, use - for stderr"`
+	LogFileName           string          `short:"l" long:"log-file" default:"-" description:"Log filename, use - for stderr"`
+	Senders               int             `short:"s" long:"senders" default:"1000" description:"Number of send goroutines to use"`
+	Debug                 bool            `long:"debug" description:"Include debug fields in the output."`
+	Flush                 bool            `long:"flush" description:"Flush after each line of output."`
+	GOMAXPROCS            int             `long:"gomaxprocs" default:"0" description:"Set GOMAXPROCS"`
+	ConnectionsPerHost    int             `long:"connections-per-host" default:"1" description:"Number of times to connect to each host (results in more output)"`
+	ReadLimitPerHost      int             `long:"read-limit-per-host" default:"96" description:"Maximum total kilobytes to read for a single host (default 96kb)"`
+	Prometheus            string          `long:"prometheus" description:"Address to use for Prometheus server (e.g. localhost:8080). If empty, Prometheus is disabled."`
+	CustomDNS             string          `long:"dns" description:"Address of a custom DNS server for lookups. Default port is 53."`
+	Multiple              MultipleCommand `command:"multiple" description:"Multiple module actions"`
+	inputFile             *os.File
+	outputFile            *os.File
+	metaFile              *os.File
+	logFile               *os.File
+	inputTargets          InputTargetsFunc
+	outputResults         OutputResultsFunc
+	localAddr             *net.TCPAddr
+	statusUpdatesFile     *os.File
+	BlocklistFileName     string        `short:"b" long:"blocklist-file" default:"-" description:"Blocklist filename, use - for $(HOME)/.config/zgrab2/blocklist.conf"`
+	StatusUpdatesFileName string        `short:"u" long:"status-updates-file" default:"-" description:"Status updates filename, use - for stderr."`
+	LocalPortString       string        `long:"local-port" description:"Local port(s) to bind to for outgoing connections. Comma-separated list of ports or port ranges (inclusive) ex: 1200-1300,2000"`
+	DNSResolutionTimeout  time.Duration `long:"dns-resolution-timeout" default:"10s" description:"Timeout for DNS resolution of target hostnames. Default is 10 seconds."`
+	UserIPv4Choice        *bool         `long:"resolve-ipv4" description:"Use IPv4 for resolving domains (accept A records). True by default, use only --resolve-ipv6 for IPv6 only resolution. If used with --resolve-ipv6, will use both IPv4 and IPv6."`
+	UserIPv6Choice        *bool         `long:"resolve-ipv6" description:"Use IPv6 for resolving domains (accept AAAA records). IPv6 is disabled by default. If --resolve-ipv4 is not set and --resolve-ipv6 is, will only use IPv6. If used with --resolve-ipv4, will use both IPv4 and IPv6."`
+	customDNSNameservers  []string      // will be non-empty if user specified custom DNS, we'll check these are reachable before populating
+	localAddrs            []net.IP      // will be non-empty if user specified local addresses
+	localPorts            []uint16      // will be non-empty if user specified local ports
+	resolveIPv4           bool          // true if IPv4 is enabled, false if only IPv6 is enabled. Guaranteed to be set, whereas UserIPv4Choice may be nil if unset by the user
+	resolveIPv6           bool
 }
 
 // SetInputFunc sets the target input function to the provided function.
@@ -108,7 +120,7 @@ func validateFrameworkConfiguration() {
 	} else if len(config.MetaFileName) > 0 {
 		var err error
 		if config.metaFile, err = os.Create(config.MetaFileName); err != nil {
-			log.Fatal(err)
+			log.Fatal(fmt.Errorf("error creating meta file: %w", err))
 		}
 	}
 
